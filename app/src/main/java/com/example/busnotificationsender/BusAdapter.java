2e355c4bd6ebbc3efa304ad9afe5ad835f2fc7aa
Abhay Maniyar <abhaymaniyarhack@gmail.com>
2017-04-16 16:00:31 +0530

    GetEditTextUsingTag and TextWatcher method

--- app/src/main/java/com/example/busnotificationsender/BusAdapter.java
+++ app/src/main/java/com/example/busnotificationsender/BusAdapter.java
@@ -2,6 +2,8 @@ package com.example.busnotificationsender;
 
 import android.app.Activity;
 import android.graphics.Typeface;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,25 +12,33 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by abhay on 6/4/17.
  */
 
 public class BusAdapter extends ArrayAdapter<Bus> {
+
+    private HashMap<String, Integer> textValues = new HashMap<String, Integer>();
+
     public BusAdapter(Activity context, ArrayList<Bus> buses){
         super(context, 0, buses);
     }
 
     public View getView(int position, View convertView, ViewGroup parent){
         View busListItemView = convertView;
+        boolean convertViewWasNull = false;
         if(busListItemView == null) {
             busListItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+            convertViewWasNull = true;
         }
 
         //        EditText to enter the Bus number
         Bus currentBus = getItem(position);
         EditText editText = (EditText) busListItemView.findViewById(R.id.busNumber);
+        //be aware that you shouldn't do this for each call on getView, just once by listItem when convertView is null
+        editText.addTextChangedListener(new GenericTextWatcher(editText));
 
         //        Used EraserRegular font for the bus-stop names
         Typeface typeFace = Typeface.createFromAsset(getContext().getAssets(), "fonts/KGSecondChancesSolid.ttf");
@@ -39,5 +49,30 @@ public class BusAdapter extends ArrayAdapter<Bus> {
         destinationsTextView.setText(currentBus.getDestinations());
         destinationsTextView.setTypeface(typeFace);
         return busListItemView;
+    }
+
+    private class GenericTextWatcher implements TextWatcher {
+
+        private View view;
+        private GenericTextWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+        public void afterTextChanged(Editable editable) {
+
+            String text = editable.toString();
+            //save the value for the given tag :
+            BusAdapter.this.textValues.put(view.getTag().toString(), Integer.parseInt(editable.toString()));
+        }
+    }
+
+//    you can implement a method like this one for each EditText with the list position as parameter :
+    public Integer getValueFromEditText(int position){
+        //here you need to recreate the id for the editText
+        Integer result = textValues.get("theFirstEditTextAtPos:"+position);
+        return result;
     }
 }
--- app/src/main/java/com/example/busnotificationsender/MainActivity.java
+++ app/src/main/java/com/example/busnotificationsender/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -15,25 +14,12 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DatabaseReference mDatabase =  FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        Bus bus1 = new Bus("Vijaynagar", 32);
-//        Map<String, Integer> buses = new HashMap<String, Integer>();
-//        buses.put("Pardesipura", 1);
-//        buses.put("Sai ", 11);
-//        buses.put("Ram", 31);
-//        buses.put("sapna", 311);
-//        buses.put("asf", 1);
-//        buses.put("Pardasdfesipura", 1);
-//        buses.put("Pardesipura", 1);
-//        buses.put("iii", 41);
-//        buses.put("asdfasdf", 1);
-//        buses.put("asdfasdfere", 31);
-//
 
         final ArrayList<Bus> buses = new ArrayList<Bus>();
         buses.add(new Bus("देव्गुरदिया इंडस्टृी होउस पलासिया", 0));
@@ -48,24 +34,38 @@ public class MainActivity extends AppCompatActivity {
         buses.add(new Bus("शालीमार स्कीम खन्डवा नाका", 0));
         buses.add(new Bus("तीनd ईमली खजराना बोय्ज होस्टल", 0));
         buses.add(new Bus("खातीवdाला राजवाडा अग्निबाण रानी सती गेट", 0));
-        BusAdapter busAdapter= new BusAdapter(this, buses);
+        final BusAdapter busAdapter = new BusAdapter(this, buses);
 
         // Get a reference to the ListView, and attach the adapter to the listView.
-        ListView listView = (ListView) findViewById(R.id.busListView);
+        final ListView listView = (ListView) findViewById(R.id.busListView);
         listView.setAdapter(busAdapter);
 
 //        sends RealtimeDatabase write operation to Firebase database and shows a toast
         Button submitButton = (Button) findViewById(R.id.submit_button);
-        EditText newBusNumber = (EditText) findViewById(R.id.busNumber);
+//        EditText newBusNumber = (EditText) findViewById(R.id.busNumber);
+//        int i = Integer.valueOf(listView.getChildAt(2).findViewById(R.id.busNumber).getContentDescription().toString());
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    DatabaseReference bdRef = mDatabase.child("Buses");
-                    for (int i = 0; i < buses.size(); i++){
-                        bdRef.child(buses.get(i).getDestinations()).setValue(buses.get(i).getBusNumber());
-                    }
+                DatabaseReference bdRef = mDatabase.child("Buses");
+                for (int i = 0; i < buses.size(); i++) {
+//                    int newBusNumber = Integer.valueOf(listView.getChildAt(2).findViewById(R.id.busNumber).getContentDescription().toString());
+                    bdRef.child(buses.get(i).getDestinations()).setValue(busAdapter.getValueFromEditText(i).intValue());
+                }
                 Toast.makeText(getApplicationContext(), "Notifications Sent", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public View getViewByPosition(int position, ListView listView) {
+        final int firstListItemPosition = listView.getFirstVisiblePosition();
+        final int lastListItemPosition = firstListItemPosition + listView.getChildCount() - 1;
+
+        if (position < firstListItemPosition || position > lastListItemPosition) {
+            return listView.getAdapter().getView(position, listView.getChildAt(position), listView);
+        } else {
+            final int childIndex = position - firstListItemPosition;
+            return listView.getChildAt(childIndex);
+        }
     }
 }
